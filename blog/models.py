d7e62b5a9682766d3a4aa7c7de2a27415c2c5ab2
Ankita Joshi <joshiankita445@gmail.com>
2022-12-07 16:20:50 +0530

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+# blog_post table will be created
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # this will be a field name author_id in tale
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
